8e0f0d92a4d43a1460226d1cee77df883d094d94
NoRisk-NoFun <58681974+NoRisk-NoFun@users.noreply.github.com>
2020-03-04 13:51:51 +0100

    new Print

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
             System.out.println("Hello World");
             System.out.println("Was machen wir jetzt ?");
             System.out.println("atmen");
+            System.out.println("WoW, wir sind sehr Erfolgreich XD");
     }
 
 }
